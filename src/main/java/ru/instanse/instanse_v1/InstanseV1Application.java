0d2e5a9a4efa27b6e5b80229ff140d9ebbfd1e39
Vovan <plategi@yandex.ru>
2022-08-27 16:04:21 +0300

    first from home

---
+++ src/main/java/ru/instanse/instanse_v1/InstanseV1Application.java
@@ -0,0 +1,13 @@
+package ru.instanse.instanse_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanseV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstanseV1Application.class, args);
+	}
+
+}
