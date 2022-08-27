7af5d389e33eccd45aa6d73ebf9790f9b8ca1a01
Vovan <plategi@yandex.ru>
2022-08-27 16:12:40 +0300

    from home

--- src/main/java/ru/instanse/instanse_v1/InstanseV1Application.java
+++ src/main/java/ru/instanse/instanse_v1/InstanseV1Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InstanseV1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InstanseV1Application.class, args);
 	}
-
 }
